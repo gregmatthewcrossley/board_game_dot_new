5aa360732f05e378222498b8d048ec61981b9fe7
Greg Matthew Crossley <greg@flashforest.ca>
2021-05-18 14:42:24 -0400

    test improvements

--- card_question_set_test.rb
+++ card_question_set_test.rb
@@ -4,34 +4,52 @@ require_relative 'card_question_set'
 
 class CardQuestionSetTest < Minitest::Test
 
+  parallelize_me!
+
+  TOPICS = [
+    "Chorioactis",
+    "Francis Walsingham",
+    "Albert Einstein"
+  ]
+
   def test_requires_a_topic
     assert_raises(ArgumentError) { CardQuestionSet.new }
   end
 
   def test_set_can_be_initiated
-    assert CardQuestionSet.new("Rob Ford")
+    TOPICS.each do |t|
+      assert CardQuestionSet.new(t), msg: "Topic was '#{t}'"
+    end
   end
 
   def test_set_can_be_generated
-    assert CardQuestionSet.new("Rob Ford").generate
+    TOPICS.each do |t|
+      assert CardQuestionSet.new(t).generate, msg: "Topic was '#{t}'"
+    end
   end
 
   def test_set_has_questions_and_answers
-    assert CardQuestionSet.new("Rob Ford").generate(count: 100).questions_and_answers.count == 100
+    TOPICS.each do |t|
+      assert (CardQuestionSet.new(t).generate(count: 5).questions_and_answers.count == 5), msg: "Topic was '#{t}'"
+    end
   end
 
   def test_set_has_no_blank_questions
-    assert CardQuestionSet.new("Rob Ford").generate(count: 100).questions_and_answers.map { |q|
-      q[:question].is_a?(String) && !q[:question].empty?
-    }.all?
+    TOPICS.each do |t|
+      assert CardQuestionSet.new(t).generate(count: 100).questions_and_answers.map { |q|
+        q[:question].is_a?(String) && !q[:question].empty?
+      }.all?, msg: "Topic was '#{t}'"
+    end
   end
 
   def test_set_has_no_blank_answers
-    assert CardQuestionSet.new("Rob Ford").generate(count: 100).questions_and_answers.map { |q|
-      q[:choices].map { |c|
-        c.is_a?(String) && !c.empty?
-      }
-    }.flatten.all?  
+    TOPICS.each do |t|
+      assert CardQuestionSet.new(t).generate(count: 100).questions_and_answers.map { |q|
+        q[:choices].map { |c|
+          c.is_a?(String) && !c.empty?
+        }
+      }.flatten.all?, msg: "Topic was '#{t}'"
+    end  
   end
 
 end
