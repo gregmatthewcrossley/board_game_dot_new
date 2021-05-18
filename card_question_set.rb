58791e70e22ddda48ec7dbd85cdcae94eeccb7c7
Greg Matthew Crossley <greg@flashforest.ca>
2021-05-17 21:57:02 -0400

    improvements

--- card_question_set.rb
+++ card_question_set.rb
@@ -1,7 +1,8 @@
-require 'pry'
-require 'wikipedia'
-require "google/cloud/language"
-ENV["GOOGLE_APPLICATION_CREDENTIALS"] = "google_application_credentials.json"
+require 'pry' # for debugging
+require 'date' # for generating plausable date substitutes
+require 'wikipedia' # for retreiving Wikipedia articles
+require 'google/cloud/language' # for analyzing text
+ENV["GOOGLE_APPLICATION_CREDENTIALS"] = "google_application_credentials.json" 
 
 class CardQuestionSet
 
@@ -9,7 +10,7 @@ class CardQuestionSet
   NUMBER_OF_QUESTIONS = 100
   QUESTION_WORD_LIMIT = 50
   NUMBER_OF_MULTIPLE_CHOICES = 4
-  YEAR_SPREAD = 30 # range of years if the answers are a year
+  PLAUSABLE_YEAR_RANGE = 30 # range of years for plausable dates
   BLANK_STRING = '________'
 
   attr_reader :topic, :wikipedia_page_title, :wikipedia_page_text, :analyzed_text, :entities, :sentences
@@ -116,10 +117,14 @@ class CardQuestionSet
   end
 
   def parse_sentences
+    # @analyzed_text[:sentences].each do |sentence|
+    #   binding.pry if sentence[:text][:content].include?("Luby")
+    # end
     @sentences = @analyzed_text[:sentences]
       .map { |sentence| sentence[:text][:content]} # get the sentences
-      .select {|sentence| sentence[-1] == "."} # discard section titles
+      .select {|sentence| sentence[-1] == "."} # discard section titles (ie sentences without a period at the end)
       .select {|sentence| sentence.split.count <= QUESTION_WORD_LIMIT } # discard long sentences
+      .select {|sentence| sentence.split("=\n").count == 1 } # remove any Wikipedia headers (ie '== Illness and death ==')
   end
 
   def generate_plausable_entity_substitutes
@@ -136,7 +141,7 @@ class CardQuestionSet
     raise ArgumentError, "type must be one of: #{entity_type_whitelist.to_sentence}" unless entity_type_whitelist.include? type
     raise ArgumentError, "proper_noun must be True or False" unless [true, false].include? proper_noun
     if type == :DATE then
-      return Array.new(100).map { |i| name.to_i + rand(-(YEAR_SPREAD / 2)..(YEAR_SPREAD / 2)) }.select{ |n| n!=name.to_i }.map(&:to_s).sample(NUMBER_OF_MULTIPLE_CHOICES - 1)
+      return plausable_date_substitutes(name)
     else
       all_plausable_substitutes = all_entity_names_except(name, type, proper_noun)
       if all_plausable_substitutes.count >= (NUMBER_OF_MULTIPLE_CHOICES - 1)
@@ -147,12 +152,71 @@ class CardQuestionSet
     end
   end
 
+  def plausable_date_substitutes(date_string)
+    date_string_is_just_a_year = Proc.new { |string|
+      string.split('').count == 4 && # check that it is 4 or fewer characters
+      string.split('').map(&:to_i).sum > 0 # check that it is just numbers
+    }
+
+    date_string_is_a_month_and_a_year = Proc.new { |string| 
+      string.split(' ').count == 2 && # the string is two words separated by a space
+      Date::MONTHNAMES.compact.include?(string.split(' ').first) && # the first word is a month
+      string.split(' ').last.split('').count <= 4 && # check that it is 4 or fewer characters
+      string.split(' ').last.split('').map(&:to_i).sum > 0 # check that it is just numbers
+    }
+
+    date_string_is_a_full_date = Proc.new { |string| 
+      begin
+        Date.parse(string).is_a? Date
+      rescue
+        false
+      end
+    }
+
+    case date_string
+    when date_string_is_just_a_year
+      Array.new(100)
+        .map { |i| date_string.to_i + rand(-PLAUSABLE_YEAR_RANGE..1) }
+        .select { |d| d != date_string.to_i }
+        .map(&:to_s)
+        .uniq
+        .sample(NUMBER_OF_MULTIPLE_CHOICES - 1)
+    when date_string_is_a_month_and_a_year
+      Array.new(100)
+        .map { |i| Date.parse(date_string) + rand(-PLAUSABLE_YEAR_RANGE*365..1) }
+        .select { |d| d != Date.parse(date_string) }
+        .map { |d| d.strftime("%B %Y") }
+        .uniq
+        .sample(NUMBER_OF_MULTIPLE_CHOICES - 1)
+    when date_string_is_a_full_date
+      Array.new(100)
+        .map { |i| Date.parse(date_string) + rand(-PLAUSABLE_YEAR_RANGE*365..1) }
+        .select { |d| d != Date.parse(date_string) }
+        .map { |d| d.strftime("%B %-d, %Y") }
+        .uniq
+        .sample(NUMBER_OF_MULTIPLE_CHOICES - 1)
+    else
+      Date::MONTHNAMES.compact.sample(NUMBER_OF_MULTIPLE_CHOICES - 1) # just return some random month names
+    end
+  end
+
   def all_entity_names_except(name, type, proper_noun)
     @entities.select { |entity|
       entity[:name] != name && # drop the entity if it has the same name
       entity[:type] == type && # drop the entity unless it is the same type
       entity[:proper_noun?] == proper_noun # only keep either proper or regular nouns
-    }.map { |entity| entity[:name] }.uniq
+    }.map { |entity| entity[:name] }.uniq # remove duplicates
+    .map { |name| 
+      if proper_noun
+        if name.split(', ').count == 2 # reverse any names with commas (ie 'Smith, John')
+          name.split(', ').map(&:capitalize).reverse.join(' ')
+        else
+          name.split(' ').map(&:capitalize).join(' ')
+        end
+      else
+        name.downcase
+      end # match capitalization to noun type
+    }
   end
 
   def generate_question_phrases
@@ -164,7 +228,12 @@ class CardQuestionSet
 
   def sentence_with_blank_for(name)
     sentence = sentence_for(name)
-    return nil if sentence_for(name).nil?
+    return nil if sentence_for(name).nil? # skip any blank sentences
+    begin
+      return nil if sentence_for(name).gsub('-','~~~~~').delete(name.gsub('-','~~~~~')).split('').count == 0 # skip sentences only containing the entity name (the ~~~~ is to make sure there aren't any dashes in the string, which messed up the delete() method)
+    rescue ArgumentError
+      binding.pry
+    end
     sentence_for(name).gsub(name, BLANK_STRING)
   end
 
